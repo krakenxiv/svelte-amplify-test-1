248bb0681ebdc9dd90d76896ac6d3dcdf98db826
Old Clunker <oldclunker@Olds-MBP.nordicsemi.no>
2025-04-16 09:38:58 +0200

    adding style files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,44 @@
-<h1>Super basic Svelte - Ampplify test</h1>
+<script lang="ts">
+	import '../app.css';
+
+	let count = $state(0);
+
+	const onclickPlus = () => {
+		count = count + 1;
+	};
+
+	const onclickMinus = () => {
+		count = count - 1;
+	};
+</script>
+
+<section>
+	<h1>Super basic Svelte - Amplify test</h1>
+	<p>
+		{count}
+	</p>
+	<button onclick={onclickPlus}>+</button>
+	<button onclick={onclickMinus}>-</button>
+</section>
 
 <style>
-	body {
-		background: #333;
-		color: #fff;
-	}
-	h1 {
+	section {
 		text-align: center;
+	}
+
+	p {
+		font-size: 20px;
+		margin: 4em 20em;
+		padding: 2em;
+		border: 1px solid #555;
+	}
+
+	button {
+		background: #222;
+		border: 1px solid #555;
+		border-radius: 6px;
+		color: #ccc;
+		padding: 1em 2em;
+		margin: 2em;
 	}
 </style>
