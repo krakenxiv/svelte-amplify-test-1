651d5179db9b8731cf18f680fba2d189a528b8e9
Old Clunker <oldclunker@Olds-MBP.nordicsemi.no>
2025-04-16 11:01:17 +0200

    trying to get all files to build

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="layout">
+	<header>
+		<h1>Super basic Svelte - Amplify test</h1>
+	</header>
+	<nav class="menu">
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+			<li>
+				<a href="/services">Services</a>
+			</li>
+		</ul>
+	</nav>
+	<section>
+		{@render children()}
+	</section>
+	<footer>Footer</footer>
+</div>
